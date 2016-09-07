6840df452366fe551aab0fbf5b0b242c17021af4
Travis Cunningham <travis@travis-cunningham.com>
2016-09-06 17:06:09 -0700

    Added Checklists Controller - Successful build - Page 152

---
+++ Checklists/AllListsViewController.swift
@@ -0,0 +1,59 @@
+//
+//  AllListsViewController.swift
+//  Checklists
+//
+//  Created by Travis Cunningham on 9/6/16.
+//  Copyright © 2016 Travis Cunningham. All rights reserved.
+//
+
+import UIKit
+
+class AllListsViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 3
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = cellForTableView(tableView: tableView)
+
+        // Configure the cell...
+        cell.textLabel!.text = "List \(indexPath.row)"
+     
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "ShowChecklist", sender: nil)
+    }
+    
+    
+    func cellForTableView(tableView: UITableView) -> UITableViewCell {
+        let cellIdentifier = "Cell"
+        if let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier) {
+            return cell
+        } else {
+            return UITableViewCell(style: .default, reuseIdentifier: cellIdentifier)
+        }
+    }
+
+}
